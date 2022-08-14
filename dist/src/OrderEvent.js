fe9581005d0c2ae51b8b9c0a804052f65a79baa0
ZanJun Wang <zanjun@crypto-arsenal.io>
2022-08-14 00:25:07 -0700

    feat: liquidation

---
+++ dist/src/OrderEvent.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.OrderEvent = void 0;
+var OrderEvent;
+(function (OrderEvent) {
+    OrderEvent["LIQUIDATION"] = "LIQUIDATION";
+    OrderEvent["STOP_LOSS"] = "STOP_LOSS";
+    OrderEvent["TAKE_PROFIT"] = "TAKE_PROFIT";
+})(OrderEvent = exports.OrderEvent || (exports.OrderEvent = {}));
+//# sourceMappingURL=OrderEvent.js.map
